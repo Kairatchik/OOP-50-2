5fd5ad2614c88b270a7bd4ee814ee3d29c2aa4af
Kairatchik <tashbolotov07@gmail.com>
2025-01-07 00:17:29 +0600

    Третье задание

--- 1stlesson/1stexersise.py
+++ 1stlesson/1stexersise.py
@@ -10,15 +10,18 @@ class Hero:
     def is_adult(self):
         return self.lvl >= 10
 
+    def __str__(self):
+        return f"Имя: {self.name}, lvl: {self.lvl}, HP: {self.HP}"
+
 hero1 = Hero(name="Артур", lvl=5, HP=100)
 hero2 = Hero(name="Ланселот", lvl=12, HP=150)
 hero3 = Hero(name="Гвен", lvl=10, HP=120)
 
-hero1.introduce()
+print(hero1)
 print(hero1.is_adult())
 
-hero2.introduce()
+print(hero2)
 print(hero2.is_adult())
 
-hero3.introduce()
+print(hero3)
 print(hero3.is_adult())
