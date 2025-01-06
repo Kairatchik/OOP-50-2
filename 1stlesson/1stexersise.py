6b808c108c2a60ce248a3f7a67136b85762065c8
Kairatchik <tashbolotov07@gmail.com>
2025-01-07 00:12:54 +0600

    Первое задание

---
+++ 1stlesson/1stexersise.py
@@ -0,0 +1,11 @@
+class Hero:
+    def __init__(self, name, lvl, HP):
+        self.name = name
+        self.lvl = lvl
+        self.HP = HP
+
+    def introduce(self):
+        print(f"Привет, меня зовут {self.name}, мой lvl {self.lvl}, мое HP {self.HP}")
+
+hero = Hero(name="Артур", lvl=5, HP=100)
+hero.introduce()
