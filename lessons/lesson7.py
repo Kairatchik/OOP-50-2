f16f1e25be892cb6cdd02b5915728e4065bb91e6
Kairatchik <tashbolotov07@gmail.com>
2025-01-24 20:57:42 +0600

    lesson7update

--- lessons/lesson7.py
+++ lessons/lesson7.py
@@ -10,6 +10,7 @@ cursor = db.cursor()
 def create_table():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             name VARCHAR (20) NOT NULL,
             age INTEGER NOT NULL,
             hoby TEXT
@@ -29,7 +30,7 @@ def add_user(name, age, hoby):
     db.commit()
     print(f"Добавили {name}")
     
-# add_user("Kairat", 26, "играть")
+# add_user("Вася", 17, "катать")
 
 def get_all_users():
     cursor.execute('SELECT * FROM users')
@@ -40,8 +41,41 @@ def get_all_users():
         print('Список всех пользователей')
         for user in users:
             print(f"Name: {user[0]}, AGE: {user[1]}, HOBBY: {user[2]}")
-    else:
-        print('Список пользователей пуст')
+    # else:
+    #     print('Список пользователей пуст')
+        
+def update_user_by_rowid(name=None, age=None, hoby=None, rowid=None):
+    if name:
+        cursor.execute(
+            'UPDATE users SET name = ? WHERE rowid = ?',
+            (name, rowid)
+        )
+    if age:
+        cursor.execute(
+            'UPDATE users SET age = ? WHERE rowid = ?',
+            (age, rowid)
+        )
+    if hoby:
+        cursor.execute(
+            "UPDATE users SET hoby = ? WHERE rowid = ?",
+            (hoby, rowid)
+            
+        )
+    db.commit()
+    print('Update succes')
+
+update_user_by_rowid(name='Вася ', rowid=4)        
+update_user_by_rowid(rowid = 4, age = 10)  
+
+def delete_user_by_rowid(rowid):
+    cursor.execute(
+        "DELETE FROM users WHERE rowid = ?",
+        (rowid,)
+    )
+    db.commit()
+    print('DELETE success')
+    
+delete_user_by_rowid(2)
         
             
 get_all_users()
